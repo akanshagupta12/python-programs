67373ff096124a5d91799869da9db029614e278e
akanshagupta12 <akansha.gupta_cs18@gla.ac.in>
2020-08-17 20:23:24 +0530

    selection sort done

--- selection sort.py
+++ selection sort.py
@@ -1,13 +1,15 @@
+def selectionSort(arr):
+    for j in range(0,n-1):
+        min=j;
+        for k in range(j+1,n):
+            if(arr[k]<arr[min]):
+                min=k;
+        temp=arr[j];
+        arr[j]=arr[min];
+        arr[min]=arr[j];
+    print(*arr);
 arr=[]
 n=int(input("enter the size of array"))
 for i in range (0,n):
     arr.append(int(input()));
-for j in range(0,n-1):
-    min=j;
-    for k in range(j+1,n):
-        if(arr[k]<arr[min]):
-            min=k;
-    temp=arr[j];
-    arr[j]=arr[min];
-    arr[min]=arr[j];
-print(*arr);
+bubbleSort(arr);
